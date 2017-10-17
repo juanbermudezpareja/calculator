3d96277a24f6609a78fb5eb9e2accb246b8eceb4
Macintosh <macintosh@MacBook-Pro-de-Macintosh.local>
2017-10-17 10:01:34 +0200

    Convert to es6

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: ['./calculator.js'],
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+};
